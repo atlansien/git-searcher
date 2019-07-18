b215224d493b91e8bac2b1b11c5344c980e99d6d
atlansien <atlansien@yahoo.co.jp>
2019-07-18 11:29:58 +0900

    add:actions

--- src/store.js
+++ src/store.js
@@ -4,11 +4,28 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 import { CHANGE_KEYWORD, SEARCH } from "./store/mutation-types";
+
+function getGIFs(query) {
+  const params = encodeURIComponent(query).replace(/%20/g, `+`);
+  return fetch(
+    "http://api.giphy.com/v1/gifs/search?q=" + params + "&api_key=dc6zaTOxFJmzC"
+  ).then(res => res.json());
+}
+
 export default new Vuex.Store({
   state: {
     keyword: "",
     gifs: []
   },
   mutations: {},
-  actions: {}
+  actions: {
+    [CHANGE_KEYWORD]({ commit }, keyword) {
+      commit(CHANGE_KEYWORD, keyword);
+    },
+    [SEARCH]({ commit }, state) {
+      getGIFs(state.keyword).then(data => {
+        commit(SEARCH, data);
+      });
+    }
+  }
 });
